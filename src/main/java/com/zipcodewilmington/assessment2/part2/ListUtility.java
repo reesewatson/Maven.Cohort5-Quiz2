110009295f56f769fcebbb643e87d235f79f88b6
Reese Watson <sherrese.watson@gmail.com>
2019-02-28 06:42:51 -0500

    still working on ListUtil

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -8,6 +8,7 @@ public class ListUtility {
     List<Integer> list = new ArrayList<>();
 
     public Boolean add(Integer i) {
+        if (i == null) return list.add(null);
 
         return list.add(i);
     }
@@ -21,11 +22,29 @@ public class ListUtility {
     }
 
     public List<Integer> getUnique() {
-        return null;
+        //Pay attention to return type (looking for unique values in List<Integer>), start there
+        //Use for each loop to add each unique index to arrList
+        //If arrList does not contain(i), add(i)
+        List<Integer> arrList = new ArrayList<>();
+        for (Integer i : list) {
+            if (!arrList.contains(i)) {
+                arrList.add(i);
+            }
+        }
+        return arrList;
     }
 
     public String join() {
-        return null;
+        //Pay attention to return type, joining list elements to string
+        //Start by creating string var
+        //Include commas and spaces
+        String result = "";
+        for(int i = 0; i < list.size() - 1; i++){
+            result = result + list.get(i) + ", ";
+            }
+            result += list.get(list.size() - 1);
+
+        return result;
     }
 
     public Integer mostCommon() {
